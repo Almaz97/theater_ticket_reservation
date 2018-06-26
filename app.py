547597ac2c73f310983ce70bd6c2f0e6365647e0
Almaz <almaz.carato@gmail.com>
2018-06-26 10:35:49 +0600

    made some logic

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from flask_modus import Modus
+import psycopg2
+from models import *
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from datetime import datetime
 
 app = Flask(__name__)
+modus = Modus(app)
+app.config['SECRET_KEY'] = 'this_should_be_a_secret'
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'postgresql://almaz:Almaz@localhost/reservation'
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+admin = Admin(app)
 
 @app.route('/')
 def index():
@@ -8,15 +26,71 @@ def index():
 
 @app.route('/theater')
 def theater():
-    return render_template('theater.html')
+    theaters = Theater.query.all()
+    return render_template('theater.html', theaters=theaters)
+
+@app.route('/theater/<int:id_t>')
+def th(id_t):
+
+    specs = Spectacle.query.filter_by(id_theater=id_t)
+    return render_template('spectacles.html', specs=specs)
 
 @app.route('/spectacle')
 def spectacle():
-    return render_template('spectacle.html')
+    specs = Spectacle.query.all()
+    return render_template('spectacle.html', specs=specs)
 
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
+
+@app.route('/reservation/<int:id_spec>', methods=['GET', 'POST'])
+def reservation(id_spec):
+    if request.method == 'POST':
+        qset = Spectacle.query.filter_by(id=id_spec).first()
+        theater_id = qset.id_theater
+        cur_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        scr = Screening.query.filter((Screening.date_time>cur_time) & (Screening.id_spectacle==id_spec)).first()
+
+        scr_id = scr.id_spectacle
+
+        name = request.form.get('name')
+        phone = request.form.get('phone')
+        row = request.form.get('row')
+        seat = request.form.get('seat')
+
+        new_reservation = Reservation(
+            id_theater=theater_id, id_screening=scr_id, reservation_name=name,
+            reservation_number=phone, active=True, paid=False
+        )
+
+        db.session.add(new_reservation)
+        db.session.commit()
+
+        id_res = db.session.query(func.count(Reservation.id)).scalar()
+
+        new_seat_reserved = SeatReserved(
+            id_screening=scr_id, id_reservation=id_res, number=seat, row=row
+        )
+
+        db.session.add(new_seat_reserved)
+        db.session.commit()
+
+        return render_template('succed.html')
+
+    info = Spectacle.query.filter_by(id=id_spec).first()
+    date_info = Screening.query.filter_by(id_spectacle=id_spec).first()
+    return render_template('reservation.html', info=info, date_info=date_info)
+
+#Admin
+admin.add_view(ModelView(Administrators, db.session))
+admin.add_view(ModelView(Theater, db.session))
+admin.add_view(ModelView(Auditorium, db.session))
+admin.add_view(ModelView(Spectacle, db.session))
+admin.add_view(ModelView(Screening, db.session))
+admin.add_view(ModelView(Reservation, db.session))
+admin.add_view(ModelView(SeatReserved, db.session))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,1 +1,98 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_modus import Modus
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this_should_be_a_secret'
+mosus = Modus(app)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'postgresql://almaz:Almaz@localhost/reservation'
+
+# DB models
+
+class Administrators(db.Model):
+    __tablename__ = 'Administrators'
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.Text)
+    second = db.Column(db.Text)
+    username = db.Column(db.Text)
+    password = db.Column(db.Text)
+
+class Theater(db.Model):
+    __tablename__ = 'Theater'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    am_auditorium = db.Column(db.Integer)
+    address = db.Column(db.Text)
+    description = db.Column(db.Text)
+
+    #relationships
+    auditorium = db.relationship('Auditorium', backref='theater')
+    spectacle = db.relationship('Spectacle', backref='theater')
+    screening = db.relationship('Screening', backref='theater')
+    reservation = db.relationship('Reservation', backref='theater')
+
+    def __str__(self):
+        return self.name
+
+class Auditorium(db.Model):
+    __tablename__ = 'Auditorium'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    id_theater = db.Column(db.Integer, db.ForeignKey('Theater.id')) #FK
+    am_seats = db.Column(db.Integer)
+
+    #relationships
+    screening = db.relationship('Screening', backref='auditorium')
+
+    def __str__(self):
+        return self.name
+
+class Spectacle(db.Model):
+    __tablename__ = 'Spectacle'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    id_theater = db.Column(db.Integer, db.ForeignKey('Theater.id')) #FK
+    description = db.Column(db.Text)
+
+    #relationships
+    spectacle = db.relationship('Screening', backref='spectacle')
+
+    def __str__(self):
+        return self.title
+
+class Screening(db.Model):
+    __tablename__ = 'Screening'
+    id = db.Column(db.Integer, primary_key=True)
+    id_auditorium = db.Column(db.Integer, db.ForeignKey('Auditorium.id')) #FK
+    id_spectacle = db.Column(db.Integer, db.ForeignKey('Spectacle.id')) #FK
+    id_theater = db.Column(db.Integer, db.ForeignKey('Theater.id')) #FK
+    date_time = db.Column(db.DateTime)
+
+    #relationships
+    reservation = db.relationship('Reservation', backref='screening')
+    seat_reserved = db.relationship('SeatReserved', backref='screening')
+
+
+
+class Reservation(db.Model):
+    __tablename__ = 'Reservation'
+    id = db.Column(db.Integer, primary_key=True)
+    id_theater = db.Column(db.Integer, db.ForeignKey('Theater.id')) #FK
+    id_screening = db.Column(db.Integer, db.ForeignKey('Screening.id')) #FK
+    reservation_name = db.Column(db.Text)
+    reservation_number = db.Column(db.Integer)
+    active = db.Column(db.Boolean)
+    paid = db.Column(db.Boolean)
+
+    #relationships
+    seat_reserved = db.relationship('SeatReserved', backref='reservation')
+
+class SeatReserved(db.Model):
+    __tablename__ = 'SeatReserved'
+    id = db.Column(db.Integer, primary_key=True)
+    id_screening = db.Column(db.Integer, db.ForeignKey('Screening.id')) #FK
+    id_reservation = db.Column(db.Integer, db.ForeignKey('Reservation.id')) #FK
+    number = db.Column(db.Integer)
+    row = db.Column(db.Integer)
